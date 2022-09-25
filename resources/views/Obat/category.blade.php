b04269067c92cbf2764a2cca2420716b5452d3d5
githubYubella <anmpbella@gmail.com>
2022-09-26 00:42:06 +0700

    css Category

--- resources/views/Obat/category.blade.php
+++ resources/views/Obat/category.blade.php
@@ -5,25 +5,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <title>Document</title>
 </head>
 
 <body>
-    <table>
-        <tr>
-            <th>ID</th>
-            <th>Nama</th>
-        </tr>
-        @foreach($data as $d)
-        <tr>
-            <td>{{$d -> id}}</td>
-            <td>{{$d -> nama}}</td>
+    <div class="container">
+        <h2>Product Table</h2>
+        <p>The .table-hover class enables a hover state on table rows:</p>
+        <table class="table table-hover">
+            <tr>
+                <th>ID</th>
+                <th>Nama</th>
+            </tr>
+            @foreach($data as $d)
+            <tr>
+                <td>{{$d -> id}}</td>
+                <td>{{$d -> nama}}</td>
 
 
-        </tr>
-        @endforeach
-    </table>
+            </tr>
+            @endforeach
+        </table>
+    </div>
 
 </body>
 
